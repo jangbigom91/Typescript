2f4c960b409dd73396415073995ad691a936837c
jangbigom91 <jangbigom91@gmail.com>
2021-01-12 14:19:25 +0900

    app2 type annotations

---
+++ app2.ts
@@ -0,0 +1,17 @@
+let studentID:number = 12345;
+let studentName:string = 'Jenny Kim';
+let age:number = 21;
+let gender:string = 'male';
+let subject:string = 'Javascript';
+let courseCompleted:boolean = false;
+
+function getStudentDetails(studentID:number):{
+   studentID: number;
+   studentName: string;
+   age: number;
+   gender: string;
+   subject: string;
+   createDate: Date; 
+} {
+    return null;
+}
